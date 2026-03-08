e875e7d2249d40c5de8635dd7bbb9b3189bb6298
Jose <ga0651032023@unab.edu.sv>
2026-03-07 19:18:59 -0600

    feat: agregar edicion de productos

--- apps/app.js
+++ apps/app.js
@@ -1,6 +1,32 @@
-import { renderProductos } from "./render.js";
-import { agregarProducto } from "./agregar.js";
+import { renderProductos, cargarCategorias } from "./render.js";
+
+import {
+agregarProducto,
+editarProducto,
+cancelarEdicion,
+eliminarProducto,
+verDetallesProducto
+} from "./agregar.js";
 
 window.agregarProducto = agregarProducto;
+window.editarProducto = editarProducto;
+window.cancelarEdicion = cancelarEdicion;
+window.eliminarProducto = eliminarProducto;
+window.verDetallesProducto = verDetallesProducto;
 
-document.addEventListener("DOMContentLoaded", renderProductos);
+window.mostrarSeccion = function(seccion){
+
+document.getElementById("seccionCrear")
+.classList.toggle("oculto",seccion!=="crear");
+
+document.getElementById("seccionListar")
+.classList.toggle("oculto",seccion!=="listar");
+
+};
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+cargarCategorias();
+renderProductos();
+
+});
